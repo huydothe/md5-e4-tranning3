e8704548b0b16fe0b0cea7973edf4cce24b0e099
huydothe <you@example.com>
2022-10-03 17:28:27 +0700

    bt;

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const React = require('react');
+const ReactDom = require('react-dom/client');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const fruits = ["Apple","Orange","Apricot","Black rowan","Cranberry"];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let fruit = <div>
+    <h1>
+        List of fruits
+    </h1>
+    <ul>
+        {fruits.map((item)=>(
+            <li>{item}</li>
+        ))}
+    </ul>
+</div>
+
+let root = ReactDom.createRoot(document.getElementById('root'));
+root.render(fruit);
